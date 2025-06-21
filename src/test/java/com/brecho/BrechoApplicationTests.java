107e2d9bb2d5f63c0c0bda297e4c3d5d11b06b8c
Taris Henrique <taris.tj@hotmail.com>
2025-06-20 22:01:48 -0300

    primeiro commit arquivos básicos do projeto

---
+++ src/test/java/com/brecho/BrechoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brecho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrechoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
